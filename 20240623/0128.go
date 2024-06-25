7af199c078e5be5d9a6f711d3a039fa04d2c926d
GaryGogh <Chongge.Gao@mail.dlut.edu.cn>
2024-06-25 23:41:26 +0800

    0625 practice

--- 20240623/0128.go
+++ 20240623/0128.go
@@ -1,1 +1,21 @@
-package main
+func longestConsecutive(nums []int) int {
+    HM:=map[int]bool {}
+    for _,i:=range nums{
+        HM[i]=true
+        
+    }
+    res:=0
+    for num:=range HM{
+        if !HM[num-1]{
+            cur:=1
+           for HM[num+1]{
+                num+=1
+                cur+=1           
+           } 
+           if res<cur{
+            res=cur
+           }
+        }
+    }
+    return res
+}
